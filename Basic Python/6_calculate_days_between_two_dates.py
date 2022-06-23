39b728453dbb85acb667da8846ff21c2cf53b874
chakravarthy <m.chakra473@gmail.com>
2022-06-23 17:55:20 +0530

    [Chakravarthy]Add: 6.Calculates the Days Between Two Dates

---
+++ Basic Python/6_calculate_days_between_two_dates.py
@@ -0,0 +1,38 @@
+"""
+    @Author: Chakravarthy
+    @Date: 2022-06-23 14:13:07
+    @Last Modified by: Chakravarthy
+    @Last Modified time: 2022-06-23 14:13:15
+    @Title : Python Code for Calculating number of days between two days
+"""
+from datetime import date
+import logging
+
+# Create and configure logger
+logging.basicConfig(filename="newfile.log",
+                    format='%(asctime)s %(message)s',
+                    filemode='w')
+logger = logging.getLogger()
+# Setting the threshold of logger to DEBUG
+logger.setLevel(logging.DEBUG)
+
+
+def calculate_no_of_days_between_dates(year1, month1, day1, year2, month2, day2):
+    f_date = date(year1, month1, day1)
+    l_date = date(year2, month2, day2)
+    delta = l_date - f_date
+    logger.info("code is successfully ran")
+    return delta.days
+
+
+if __name__ == "__main__":
+    try:
+        a, b, c = (input("enter the first date in Year month day format: ").split())
+        x, y, z = (input("enter the last date in Year month day format: ").split())
+        x, y, z = [int(i) for i in [x, y, z]]
+        a, b, c = [int(j) for j in [a, b, c]]
+        print(f"Difference between these two dates \"{a}-{b}-{c}\" and \"{x}-{y}-{z}\" is"
+              f" \"{calculate_no_of_days_between_dates(a, b, c, x, y, z)}\"")
+    except ValueError as err:
+        print(err)
+        logger.error(err)
