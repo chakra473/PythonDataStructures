ab0d598ce8be40ab21f532f8d262c3da2fd8c9c1
chakravarthy <m.chakra473@gmail.com>
2022-06-23 21:58:11 +0530

    initial commit

--- Basic Python/6_calculate_days_between_two_dates.py
+++ Basic Python/6_calculate_days_between_two_dates.py
@@ -21,12 +21,16 @@ def calculate_no_of_days_between_dates(year1, month1, day1, year2, month2, day2)
     f_date = date(year1, month1, day1)
     l_date = date(year2, month2, day2)
     delta = l_date - f_date
-    logger.info("code is successfully ran")
+    logger.info(f"Difference between these two dates \"{a}-{b}-{c}\" and \"{x}-{y}-{z}\" is {delta.days}")
     return delta.days
 
 
 if __name__ == "__main__":
     try:
+        title_name = "Python Code for Calculating number of days between two days"
+        user_name = input("enter your name: ")
+        logger.info(f"TITLE\n{title_name}")
+        logger.info(f"user name who is running the code is {user_name}")
         a, b, c = (input("enter the first date in Year month day format: ").split())
         x, y, z = (input("enter the last date in Year month day format: ").split())
         x, y, z = [int(i) for i in [x, y, z]]
