33231f0b6bb88cc5020210476ea8083978115c54
chakravarthy <m.chakra473@gmail.com>
2022-06-23 21:53:12 +0530

    [Chakravarthy]Add: 7.Check Value Contain in a List

--- Basic Python/7_check_value_contain_in_group_list.py
+++ Basic Python/7_check_value_contain_in_group_list.py
@@ -10,7 +10,7 @@ import logging
 # Create and configure logger
 logging.basicConfig(filename="newfile.log",
                     format='%(asctime)s %(message)s',
-                    filemode='w')
+                    filemode='a')
 logger = logging.getLogger()
 # Setting the threshold of logger to DEBUG
 logger.setLevel(logging.DEBUG)
@@ -29,16 +29,18 @@ def check_value_in_list(list_of_num=None, value=None):
 
 if __name__ == "__main__":
     try:
+        title_name = "Python Code for Checking Specified Value present in group of Values"
         user_name = input("enter your name: ")
+        logger.info(f"TITLE\n{title_name}")
+        logger.info(f"user name who is running the code is {user_name}")
         list1 = []
         # number of elements as input
         n = int(input("Enter number of elements : "))
         # iterating till the range
         for j in range(0, n):
-            ele = int(input("enter the group of value: "))
+            ele = int(input(f"enter the value{j+1}: "))
             list1.append(ele)  # adding the element
         value1 = int(input("enter the value to be checked in the list: "))
-        logger.info(f"user name who is running the code is {user_name}")
         logger.info(f"list of values user enters is {list1}")
         print(check_value_in_list(list1, value1))
     except ValueError as err:
