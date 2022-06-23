59aa10be850bf4b216313c099b15bff902736e84
chakravarthy <m.chakra473@gmail.com>
2022-06-23 16:03:26 +0530

    [Chakravarthy]Add: 4.Prints documents(syntax,dsecription) of Builtin Python Function

---
+++ Basic Python/4.print_docs_python_inbuilt_func.py
@@ -0,0 +1,16 @@
+"""
+    @Author: Chakravarthy
+    @Date: 2022-06-23 14:13:07
+    @Last Modified by: Chakravarthy
+    @Last Modified time: 2022-06-23 14:13:15
+    @Title : Python Code for printing documents of python inbuilt functions
+"""
+
+
+def print_docs_inbuilt_func(*args):
+    for number in args:
+        print(number)
+
+
+if __name__ == "__main__":
+    print_docs_inbuilt_func(abs.__doc__, abs(5), int.__doc__, range.__doc__, list.__module__)
