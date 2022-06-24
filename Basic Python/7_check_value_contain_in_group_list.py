493738ae88e0eb7f98254019801a5f20e541376d
chakravarthy <m.chakra473@gmail.com>
2022-06-24 13:11:45 +0530

    [Chakravarthy]Refactor: 7.Check Value Contain In a List

--- Basic Python/7_check_value_contain_in_group_list.py
+++ Basic Python/7_check_value_contain_in_group_list.py
@@ -5,25 +5,17 @@
     @Last Modified time: 2022-06-23 14:13:15
     @Title : Python Code for Checking Specified Value present in group of Values
 """
-import logging
-
-# Create and configure logger
-logging.basicConfig(filename="newfile.log",
-                    format='%(asctime)s %(message)s',
-                    filemode='a')
-logger = logging.getLogger()
-# Setting the threshold of logger to DEBUG
-logger.setLevel(logging.DEBUG)
+import logger_config
 
 
 def check_value_in_list(list_of_num=None, value=None):
     print(value)
     for i in range(len(list_of_num)):
         if list_of_num[i] == value:
-            logger.info(f"the given value {value} is present in the group of values")
+            logger_config.logger.info(f"the given value {value} is present in the group of values")
             return True
 
-    logger.info(f"the given value {value} is not present in the group of values")
+    logger_config.logger.info(f"the given value {value} is not present in the group of values")
     return False
 
 
@@ -31,8 +23,8 @@ if __name__ == "__main__":
     try:
         title_name = "Python Code for Checking Specified Value present in group of Values"
         user_name = input("enter your name: ")
-        logger.info(f"TITLE\n{title_name}")
-        logger.info(f"user name who is running the code is {user_name}")
+        logger_config.logger.info(f"TITLE\n{title_name}")
+        logger_config.logger.info(f"user name who is running the code is {user_name}")
         list1 = []
         # number of elements as input
         n = int(input("Enter number of elements : "))
@@ -41,7 +33,7 @@ if __name__ == "__main__":
             ele = int(input(f"enter the value{j+1}: "))
             list1.append(ele)  # adding the element
         value1 = int(input("enter the value to be checked in the list: "))
-        logger.info(f"list of values user enters is {list1}")
+        logger_config.logger.info(f"list of values user enters is {list1}")
         print(check_value_in_list(list1, value1))
     except ValueError as err:
-        logger.warning(err)
+        logger_config.logger.warning(err)
