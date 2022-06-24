00e85153cf792286a1beab1f2b846796ff63bc01
chakravarthy <m.chakra473@gmail.com>
2022-06-24 13:33:02 +0530

    [Chakravarthy]Refactor: 6.Calculate Days Between Two Dates

--- Basic Python/6_calculate_days_between_two_dates.py
+++ Basic Python/6_calculate_days_between_two_dates.py
@@ -6,22 +6,14 @@
     @Title : Python Code for Calculating number of days between two days
 """
 from datetime import date
-import logging
-
-# Create and configure logger
-logging.basicConfig(filename="newfile.log",
-                    format='%(asctime)s %(message)s',
-                    filemode='w')
-logger = logging.getLogger()
-# Setting the threshold of logger to DEBUG
-logger.setLevel(logging.DEBUG)
+import logger_config
 
 
 def calculate_no_of_days_between_dates(year1, month1, day1, year2, month2, day2):
     f_date = date(year1, month1, day1)
     l_date = date(year2, month2, day2)
     delta = l_date - f_date
-    logger.info(f"Difference between these two dates \"{a}-{b}-{c}\" and \"{x}-{y}-{z}\" is {delta.days}")
+    logger_config.logger.info(f"Difference between these two dates \"{a}-{b}-{c}\" and \"{x}-{y}-{z}\" is {delta.days}")
     return delta.days
 
 
@@ -29,15 +21,13 @@ if __name__ == "__main__":
     try:
         title_name = "Python Code for Calculating number of days between two days"
         user_name = input("enter your name: ")
-        logger.info(f"TITLE\n{title_name}")
-        logger.info(f"user name who is running the code is {user_name}")
-        a, b, c = (input("enter the first date in Year month day format: ").split())
-        x, y, z = (input("enter the last date in Year month day format: ").split())
+        logger_config.logger.info(f"TITLE\n{title_name}")
+        logger_config.logger.info(f"user name who is running the code is {user_name}")
+        a, b, c = (input("enter the first date in YYYY MM DD format: ").split())
+        x, y, z = (input("enter the last date in YYYY MM DD format: ").split())
         x, y, z = [int(i) for i in [x, y, z]]
         a, b, c = [int(j) for j in [a, b, c]]
-        logger.info("input is correct")
-        print(f"Difference between these two dates \"{a}-{b}-{c}\" and \"{x}-{y}-{z}\" is"
-              f" \"{calculate_no_of_days_between_dates(a, b, c, x, y, z)}\"")
+        logger_config.logger.info("input is correct")
+        calculate_no_of_days_between_dates(a, b, c, x, y, z)
     except ValueError as err:
-        print(err)
-        logger.error(f"The error is :{err}")
+        logger_config.logger.error(f"The error is :{err}")
