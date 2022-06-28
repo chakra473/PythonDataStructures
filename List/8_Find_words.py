ee90999d4c19b5746f4570aaca0fda1f316e43e3
chakravarthy <m.chakra473@gmail.com>
2022-06-28 08:23:56 +0530

    [Chakravarthy]Add: Python program to  find the list of words that are longer than n from a given list of words

--- List/8_Find_words.py
+++ List/8_Find_words.py
@@ -1,17 +1,32 @@
-'''
-    @Author: Madhavee Kadivar
-    @Date: 2022-05-17 01:36:43
-    @Last Modified by: Madhavee Kadivar
-    @Last Modified time: 2022-05-17 01:36:43
-    @Title : Find words by length
-'''
-from createlist import *
-from LoggingFile import *
-logger  = func()
+"""
+    @Author: Chakravarthy
+    @Date: 2022-06-26 14:13:07
+    @Last Modified by: Chakravarthy
+    @Last Modified time: 2022-06-26 14:13:15
+    @Title : Python Code to find word by length from list
+"""
+import logger_config
+import create_list
+
+title_name = "Python Code to find word by length from list"
+user_name = input("enter your name: ")
+logger_config.logger.info(f"TITLE\n{title_name}")
+logger_config.logger.info(f"user name who is running the code is {user_name}")
+
+
+def find_words(list1):
+    """
+    This method gives elements which are longer than length
+    :param list1:list of elements
+    :return:list with elements longer than length
+    """
+    length = int(input("Enter any length value : "))
+    new_list = [i for i in list1 if len(i) > length]
+    logger_config.logger.info(f"All element having length longer than {length} are : {new_list}")
+    return new_list
+
 
 if __name__ == "__main__":
-    list_element = create_string_list()
-    logger.info(list_element)
-    length = int(input("Enter any length value : "))
-    new_list = [i for i in list_element if len(i) > length ]
-    logger.info(f"All element having length longer than {length} are : {new_list}")
+    list_element = create_list.create_string_list()
+    logger_config.logger.info(f"list of values user enters is {list_element}")
+    find_words(list_element)
