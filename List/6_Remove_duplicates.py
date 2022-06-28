94e1648bd98db58d521c729a9ce3e6d40c900e35
chakravarthy <m.chakra473@gmail.com>
2022-06-28 08:22:59 +0530

    [Chakravarthy]Add: Python program to remove duplicates from a list

--- List/6_Remove_duplicates.py
+++ List/6_Remove_duplicates.py
@@ -1,23 +1,46 @@
-'''
-    @Author: Madhavee Kadivar
-    @Date: 2022-05-17 01:06:17
-    @Last Modified by: Madhavee Kadivar
-    @Last Modified time: 2022-05-17 01:06:17
-    @Title : Remove duplicates from list 
-'''
-from createlist import *
-from LoggingFile import *
-logger  = func()
+"""
+    @Author: Chakravarthy
+    @Date: 2022-06-26 14:13:07
+    @Last Modified by: Chakravarthy
+    @Last Modified time: 2022-06-26 14:13:15
+    @Title : Python Code to remove duplicates from list
+"""
+import logger_config
+import create_list
 
-if __name__ == "__main__":
-    
-    list_element = create_string_list()
-    
-    # Method 1
+title_name = "Python Code to remove duplicates from list"
+user_name = input("enter your name: ")
+logger_config.logger.info(f"TITLE\n{title_name}")
+logger_config.logger.info(f"user name who is running the code is {user_name}")
+
+
+def remove_duplicate(list_element):
+    """
+    This method removes duplicates in the list
+    :param list_element: list of elements without duplicates
+    """
     new_list_1 = []
     [new_list_1.append(i) for i in list_element if i not in new_list_1]
-    logger.info(f"\nNew List after removing duplicates : {new_list_1}")
-    
-    # Method 2
+    logger_config.logger.info(f"New List after removing duplicates : {new_list_1}")
+    return new_list_1
+
+
+def remove_duplicate_using_set(list_element):
+    """
+    This method removes duplicates in the list using set
+    :param list_element: list of elements without duplicates
+    """
     new_list_2 = list(set(list_element))
-    logger.info(f"\nNew List after removing duplicates : {new_list_2}") 
+    logger_config.logger.info(f"New List after removing duplicates : {new_list_2}")
+    return new_list_2
+
+
+def main():
+    list_element = create_list.create_string_list()
+    logger_config.logger.info(f"list of values user enters is {list_element}")
+    remove_duplicate(list_element)
+    remove_duplicate_using_set(list_element)
+
+
+if __name__ == "__main__":
+    main()
