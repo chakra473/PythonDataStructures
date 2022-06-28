1c2bbe67034f80cc15cd3b947dbf31f9e415a2e4
chakravarthy <m.chakra473@gmail.com>
2022-06-28 08:25:14 +0530

    [Chakravarthy]Add: Python program to print a specified list after removing the 0th, 4th and 5th elements.

--- List/10_Remove_specific_element.py
+++ List/10_Remove_specific_element.py
@@ -1,20 +1,26 @@
-'''
-    @Author: Madhavee Kadivar
-    @Date: 2022-05-17 02:13:37
-    @Last Modified by: Madhavee Kadivar
-    @Last Modified time: 2022-05-17 02:13:37
-    @Title : Clone or copy list
-'''
-from createlist import *
-from LoggingFile import *
-logger  = func()
+"""
+    @Author: Chakravarthy
+    @Date: 2022-06-26 14:13:07
+    @Last Modified by: Chakravarthy
+    @Last Modified time: 2022-06-26 14:13:15
+    @Title : Python Code to find word by length from list
+"""
+import logger_config
+import create_list
+
+title_name = "Python Code to find word by length from list"
+user_name = input("enter your name: ")
+logger_config.logger.info(f"TITLE\n{title_name}")
+logger_config.logger.info(f"user name who is running the code is {user_name}")
+
+
 if __name__ == "__main__":
-    list_element = create_string_list()
-    logger.info(list_element)
+    list_element = create_list.create_string_list()
+    logger_config.logger.info(list_element)
     # removing the 0th, 4th and 5th elements.
     list_element.pop(0)
-    logger.info(list_element)
+    logger_config.logger.info(list_element)
     list_element.pop(3)
-    logger.info(list_element)
+    logger_config.logger.info(list_element)
     list_element.pop(3)
-    logger.info(f"List after removing 0th,4th and 5th index element : {list_element}")
+    logger_config.logger.info(f"List after removing 0th,4th and 5th index element : {list_element}")
