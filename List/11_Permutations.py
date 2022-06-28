052a780072f2f83095bc7ae8490989b75804a382
chakravarthy <m.chakra473@gmail.com>
2022-06-28 08:25:41 +0530

    [Chakravarthy]Add: Python program to generate all permutations of a list in Python.

--- List/11_Permutations.py
+++ List/11_Permutations.py
@@ -1,12 +1,20 @@
-'''
-    @Author: Madhavee Kadivar
-    @Date: 2022-05-17 02:24:31
-    @Last Modified by: Madhavee Kadivar
-    @Last Modified time: 2022-05-17 02:24:31
-    @Title : Permutations of list
-'''
+"""
+    @Author: Chakravarthy
+    @Date: 2022-06-26 14:13:07
+    @Last Modified by: Chakravarthy
+    @Last Modified time: 2022-06-26 14:13:15
+    @Title : Python Code to  generate all permutations of a list
+"""
 import itertools
-from createlist import *
+
+import logger_config
+import create_list
+
+title_name = "Python Code to  generate all permutations of a list"
+user_name = input("enter your name: ")
+logger_config.logger.info(f"TITLE\n{title_name}")
+logger_config.logger.info(f"user name who is running the code is {user_name}")
+
 
 def permutations(list_element, result=[]):
     """ 
@@ -17,22 +25,24 @@ def permutations(list_element, result=[]):
         Return:
             Nothing
     """
-    if(len(list_element) == 0):
-        logger.info(result)
+    if len(list_element) == 0:
+        logger_config.logger.info(result)
     else:
         for i in range(len(list_element)):
-            permutations(list_element[:i] + list_element[i+1:], result + list_element[i:i+1])
+            print(result + list_element[i:i + 1])
+            permutations(list_element[:i] + list_element[i + 1:], result + list_element[i:i + 1])
 
-from LoggingFile import *
-logger  = func()
-if __name__ == "__main__":
-    list_element = create_string_list()
+
+def main():
+    list_element = create_list.create_string_list()
     # Method 1
     list_of_permutations = list(itertools.permutations(list_element))
-    logger.info("\nList of all permutations : ")
-    logger.info(list_of_permutations)
+    logger_config.logger.info("\nList of all permutations : ")
+    logger_config.logger.info(list_of_permutations)
     # Method 2
-    logger.info("\nList of all permutations : ")
+    logger_config.logger.info("\nList of all permutations : ")
     permutations(list_element)
-    
-            
+
+
+if __name__ == "__main__":
+    main()
