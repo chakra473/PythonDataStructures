80781f70a903e499e71d6b254dfd78307f3d63fc
chakravarthy <m.chakra473@gmail.com>
2022-06-28 08:27:21 +0530

    [Chakravarthy]Add: Python program to find common items from two lists

--- List/15_Common_items.py
+++ List/15_Common_items.py
@@ -1,12 +1,19 @@
-'''
-    @Author: Madhavee Kadivar
-    @Date: 2022-05-17 06:30:09
-    @Last Modified by: Madhavee Kadivar
-    @Last Modified time: 2022-05-17 06:30:09
-    @Title : Find all common element from two list
-'''
-from createlist import *
-def find_common(list_1,list_2):    
+"""
+    @Author: Chakravarthy
+    @Date: 2022-06-26 14:13:07
+    @Last Modified by: Chakravarthy
+    @Last Modified time: 2022-06-26 14:13:15
+    @Title : Python Code to find common items from two lists
+"""
+
+import logger_config as lc
+import create_list as cl
+
+title_name = "Python Code to find common items from two lists"
+lc.logger.info(f"TITLE\n{title_name}")
+
+
+def find_common(list1, list2):
     """ 
         Description: 
             This function is Finding common element from two list
@@ -15,16 +22,16 @@ def find_common(list_1,list_2):
         Return:
             returns list of common element
     """
-    result = [i for i in list_1 for j in list_2 if i == j]
-    return result
+    result1 = [i for i in list1 for j in list2 if i == j]
+    return result1
 
-from LoggingFile import *
-logger  = func()
-# Main Code
+
+def main():
+    list_1 = cl.create_string_list()
+    list_2 = cl.create_string_list()
+    result = find_common(list_1, list_2)
+    lc.logger.info(f"\n{list_1} and {list_2} have common element : {result}")
+
+
 if __name__ == "__main__":
-    list_1 = create_string_list()
-    list_2 = create_string_list()
-    result = find_common(list_1,list_2)
-    logger.info(f"\n{list_1} and {list_2} have common element : {result}")
-    
-    
+    main()
