b050bf1a12b13786a87e87727164a1175080c932
chakravarthy <m.chakra473@gmail.com>
2022-06-28 08:26:29 +0530

    [Chakravarthy]Add: Python program to append a list to the second list.

--- List/13_Append_list_in_list.py
+++ List/13_Append_list_in_list.py
@@ -1,17 +1,27 @@
-'''
-    @Author: Madhavee Kadivar
-    @Date: 2022-05-17 06:18:03
-    @Last Modified by: Madhavee Kadivar
-    @Last Modified time: 2022-05-17 06:18:03
-    @Title : Append one list in another list
-'''
-from createlist import *
-# Main Code
-from LoggingFile import *
-logger  = func()
+"""
+    @Author: Chakravarthy
+    @Date: 2022-06-26 14:13:07
+    @Last Modified by: Chakravarthy
+    @Last Modified time: 2022-06-26 14:13:15
+    @Title : Python Code to append a list to the second list
+"""
+
+import logger_config as lc
+import create_list as cl
+
+title_name = "Python Code to append a list to the second list"
+lc.logger.info(f"TITLE\n{title_name}")
+
+
+def append_list_in_list(no_of_list):
+    new_list = [cl.create_string_list() for i in range(no_of_list)]
+    lc.logger.info(f"\nList of list : {new_list}")
+
+
+def main():
+    no_of_list = int(input("\nHow many number of list you want to append : "))
+    append_list_in_list(no_of_list)
+
 
 if __name__ == "__main__":
-    no_of_list = int(input("\nHow many number of list you want to append : "))
-    new_list = [create_string_list() for i in range(no_of_list)]
-    logger.info(f"\nList of list : {new_list}")
-    
+    main()
