98cf1a5dc3fcdb987df6e4304fb75d835b8a5d2f
chakravarthy <m.chakra473@gmail.com>
2022-06-28 08:23:30 +0530

    [Chakravarthy]Add: Python program to  clone or copy a list

--- List/7_Copy_list.py
+++ List/7_Copy_list.py
@@ -1,28 +1,58 @@
-'''
-    @Author: Madhavee Kadivar
-    @Date: 2022-05-17 01:21:39
-    @Last Modified by: Madhavee Kadivar
-    @Last Modified time: 2022-05-17 01:21:39
-    @Title : Clone or copy list
-'''
-from createlist import *
-from LoggingFile import *
-logger  = func()
+"""
+    @Author: Chakravarthy
+    @Date: 2022-06-26 14:13:07
+    @Last Modified by: Chakravarthy
+    @Last Modified time: 2022-06-26 14:13:15
+    @Title : Python Code to copy list from another list
+"""
+import logger_config
+import create_list
+
+title_name = "Python Code to copy list from another list"
+user_name = input("enter your name: ")
+logger_config.logger.info(f"TITLE\n{title_name}")
+logger_config.logger.info(f"user name who is running the code is {user_name}")
+
+
+def copy_list_using_slicing(list1):
+    """
+    This method copy the list by using slicing
+    :param list1: list of elements
+    :return:copied list from list1
+    """
+    copy_list = list1[:]
+    logger_config.logger.info(copy_list)
+    return copy_list
+
+
+def copy_list_using_extend_method(list1):
+    """
+    This method copy the list by using extend method
+    :param list1: list of elements
+    :return:copied list from list1
+    """
+    copy_list = []
+    copy_list.extend(list1)
+    logger_config.logger.info(copy_list)
+    return copy_list
+
+
+def copy_list_using_copy_method(list1):
+    """
+    This method copy the list by using copy method
+    :param list1: list of elements
+    :return:copied list from list1
+    """
+    list_copy = list1.copy()
+    logger_config.logger.info(list_copy)
+    return list_copy
+
 
 if __name__ == "__main__":
-    list_element = create_string_list()
-    logger.info("\nOriginal List : ")
-    logger.info(list_element)
-    logger.info("\nCopy list : ")
-    # Method 1 Using slicing
-    copy_list = list_element[:] 
-    logger.info(copy_list)
-    # Method 2 Using extend method
-    copy_list = []
-    copy_list.extend(list_element)
-    logger.info(copy_list)
-    # Method 3 Using copy method
-    list_copy = list_element.copy()
-    logger.info(list_copy)
-    
-    
+    list_element = create_list.create_string_list()
+    logger_config.logger.info("\nOriginal List : ")
+    logger_config.logger.info(list_element)
+    logger_config.logger.info("\nCopy list : ")
+    copy_list_using_slicing(list_element)
+    copy_list_using_extend_method(list_element)
+    copy_list_using_copy_method(list_element)
